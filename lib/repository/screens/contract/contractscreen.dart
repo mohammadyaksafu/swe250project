290ac947b50bd5970a81ccea94e63fbce1c265a6
mohammadyaksafu <mohammadyaksafu@gmail.com>
2025-02-26 19:41:29 +0600

    update all

--- lib/repository/screens/contract/contractscreen.dart
+++ lib/repository/screens/contract/contractscreen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -11,34 +9,119 @@ class ContractScreen extends StatefulWidget {
 }
 
 class _ContractScreenState extends State<ContractScreen> {
+  PermissionStatus _contactPermissionStatus = PermissionStatus.denied;
 
-  Future<void> askPermission() async{
-  PermissionStatus permissionStatus=await getContactsPermissions();
+  @override
+  void initState() {
+    super.initState();
+    // Check permission status when the screen loads
+    _checkContactPermission();
+  }
 
+  // Function to check contact permission status
+  Future<void> _checkContactPermission() async {
+    final status = await Permission.contacts.status;
+    setState(() {
+      _contactPermissionStatus = status;
+    });
+  }
 
+  // Function to request contact permission
+  Future<void> _requestContactPermission() async {
+    final status = await Permission.contacts.request();
+    setState(() {
+      _contactPermissionStatus = status;
+    });
 
- }
-
-  Future<PermissionStatus> getContactsPermissions() async {
-    PermissionStatus permission =await Permission.contacts.status;
-
-    if(permission !=PermissionStatus.granted && permission !=PermissionStatus.permanentlyDenied){
-      PermissionStatus permissionStatus=await Permission.contacts.request();
-      return permissionStatus;
-    }else{
-      return permission;
+    // If permission is permanently denied, open app settings
+    if (status.isPermanentlyDenied) {
+      openAppSettings();
     }
- }
-
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact Permission"),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text("Contract page "),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display permission status
+            Text(
+              "Contact Permission Status:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              _contactPermissionStatus.toString().split('.').last,
+              style: TextStyle(
+                fontSize: 22,
+                color: _getStatusColor(_contactPermissionStatus),
+              ),
+            ),
+            SizedBox(height: 30),
+            // Button to request permission
+            ElevatedButton(
+              onPressed: _requestContactPermission,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                "Request Contact Permission",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Button to open app settings if permission is permanently denied
+            if (_contactPermissionStatus.isPermanentlyDenied)
+              ElevatedButton(
+                onPressed: openAppSettings,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  "Open App Settings",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
 
-
+  // Helper function to get color based on permission status
+  Color _getStatusColor(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.granted:
+        return Colors.green;
+      case PermissionStatus.denied:
+        return Colors.orange;
+      case PermissionStatus.permanentlyDenied:
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
 }
--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resqlink/domain/constants/appcolors.dart';
 import 'package:resqlink/repository/widgets/uihelper.dart';
 
 import '../bottomnav/bottomnavscreen.dart';
@@ -9,129 +10,142 @@ class Loginscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            /** For Top Login Images */
-            UiHelper.CustomImage(img: "loginpage.png"),
-
-            /** Container for Logo  */
-            Container(
-              height: 100,
-              width: 415,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: UiHelper.CustomImage(img: "boxsizelogo.png"),
-              ),
-            ),
-
-            /** */
-            Container(
-              height: 150,
-              width: 415,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              padding: EdgeInsets.all(20),
-              child: UiHelper.CustomText(
-                text: "Your trusted companion in emergencies!",
-                color: Colors.black,
-                fontweight: FontWeight.bold,
-                fontsize: 20.0,
-                textalignment: TextAlign.center,
-                fontfamily: "bold",
-              ),
-            ),
-
-            SizedBox(height: 20),
-            Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Container(
-                height: 200,
-                width: 350,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              /** For Top Login Images */
+              
+              UiHelper.CustomImage(img: "loginpage.png"),
+              /** Container for Logo  */
+              Container(
+                height: 220,
+                width: 415,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
+                  color: AppColors.cardbackground,
+                  borderRadius: BorderRadius.circular(20)
                 ),
+
                 child: Column(
                   children: [
-                    SizedBox(height: 10),
-                    UiHelper.CustomText(
-                      text: "Md Yak Safu",
-                      color: Colors.black,
-                      fontweight: FontWeight.w500,
-                      fontsize: 15.0,
-                      textalignment: TextAlign.center,
-                      fontfamily: "regular",
-                    ),
-                    SizedBox(height: 10),
-                    UiHelper.CustomText(
-                      text: "01642468704",
-                      color: Colors.black,
-                      fontweight: FontWeight.w500,
-                      fontsize: 15.0,
-                      textalignment: TextAlign.center,
-                      fontfamily: "regular",
-                    ),
                     SizedBox(height: 20),
-                    SizedBox(
-                      height: 50,
-                      width: 200,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>Bottomnavscreen()));
-
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Row(
-                          children: [
-                            UiHelper.CustomText(
-                              text: "Login With",
-                              color: Colors.black,
-                              fontweight: FontWeight.w500,
-                              fontsize: 15.0,
-                              textalignment: TextAlign.left,
-                              fontfamily: "bold",
-                            ),
-                            Container(
-                              height: 40,
-                              width: 80,
-                              padding: EdgeInsets.all(10),
-                              child: UiHelper.CustomImage(
-                                img: "googleimageremovebg.png",
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: UiHelper.CustomImage(img: "boxsizelogo.png"),
                     ),
                     SizedBox(height: 10),
-                    UiHelper.CustomText(
-                      text: "Login with Phone Number",
-                      color: Colors.blue,
-                      fontweight: FontWeight.bold,
-                      fontsize: 20.0,
-                      textalignment: TextAlign.center,
-                      fontfamily: "regular",
-                    ),
+                   UiHelper.CustomText(
+                        text: "Your trusted companion in emergencies!",
+                        color: Colors.black,
+                        fontweight: FontWeight.bold,
+                        fontsize: 20.0,
+                        textalignment: TextAlign.center,
+                        fontfamily: "bold",
+                      ),
+
                   ],
                 ),
+
               ),
-            ),
-          ],
+        
+        
+              /*/////////////////////////
+                 Container for Text  box
+               ////////////////////////
+               */
+
+            /*//////////////////////////////
+             A box for demo data
+             //////////////////////////////
+             */
+        
+              SizedBox(height: 10),
+              Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Container(
+                  height: 180,
+                  width: 415,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.cardbackground,
+                  ),
+                  child: Column(
+                    /**for name*/
+                    children: [
+                      SizedBox(height: 5),
+                      UiHelper.CustomText(
+                        text: "Md Yak Safu",
+                        color: Colors.black,
+                        fontweight: FontWeight.w500,
+                        fontsize: 15.0,
+                        textalignment: TextAlign.center,
+                        fontfamily: "regular",
+                      ),
+                      /**for phone number */
+                      SizedBox(height: 5),
+                      UiHelper.CustomText(
+                        text: "01642468704",
+                        color: Colors.black,
+                        fontweight: FontWeight.w500,
+                        fontsize: 15.0,
+                        textalignment: TextAlign.center,
+                        fontfamily: "regular",
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>Bottomnavscreen()));
+        
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.cardbutton,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              UiHelper.CustomText(
+                                text: "Login With",
+                                color: Colors.black,
+                                fontweight: FontWeight.w500,
+                                fontsize: 15.0,
+                                textalignment: TextAlign.left,
+                                fontfamily: "bold",
+                              ),
+                              Container(
+                                height: 40,
+                                width: 80,
+                                padding: EdgeInsets.all(10),
+                                child: UiHelper.CustomImage(
+                                  img: "googleimageremovebg.png",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      UiHelper.CustomText(
+                        text: "Login with Phone Number",
+                        color: Colors.blue,
+                        fontweight: FontWeight.bold,
+                        fontsize: 20.0,
+                        textalignment: TextAlign.center,
+                        fontfamily: "regular",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
